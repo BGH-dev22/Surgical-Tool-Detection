1bba3fa6aba88f66e7a2d402e845fd2ef10d0928
BGH-dev22 <houda.boughnam@gmail.com>
2025-05-25 19:27:45 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SurgiSafe'
-copyright = '2025, IATDS'
+copyright = '2025, IATDS,ENSAM'
 author = 'IATDS'
 
 # -- General configuration ---------------------------------------------------
